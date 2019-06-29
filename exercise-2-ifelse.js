4ce74950099eeb9edb7ee42c4f05009e387be99e
Frank Lauren <lawrain21@gmail.com>
2019-06-29 19:22:00 +0700

    latihan ifelse

---
+++ exercise-2-ifelse.js
@@ -0,0 +1,23 @@
+var nama = 'Mikael';
+var peran = '';
+
+if (peran === '') {
+  console.log('Halo ' + nama + '\nPilih peranmu untuk memulai game!');
+}
+
+else if (peran === 'Ksatria') {
+  console.log('Selamat datang di Dunia Proxytia, ' + nama + '\nHalo Ksatria '+nama+', kamu dapat menyerang dengan senjatamu!');
+}
+
+else if (peran === 'Tabib') {
+  console.log('Selamat datang di Dunia Proxytia, '+nama+'\nHalo Tabib '+nama+', kamu akan membantu temanmu yang terluka');
+}
+
+else if (peran === 'Penyihir') {
+  console.log('Selamat datang di Dunia Proxytia, '+nama+'\nHalo Penyihir '+nama+', ciptakan keajaiban yang membantu kemenanganmu!');
+}
+
+else {
+  console.log('Halo ' + nama + '\nPilih peranmu untuk memulai game!');
+}
+
